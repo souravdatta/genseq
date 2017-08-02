69e2d93ac070c2d45d75fcafb480367e4cb70c7b
Sourav Datta <sdatta@planview.com>
2017-08-02 12:12:00 +0530

    Change sequence focus from conjoinable seq to lazy one

---
+++ genseq-core.rkt
@@ -0,0 +1,66 @@
+#lang typed/racket
+
+(provide null-seq
+         null-seq?
+         Seq
+         Collection
+         s/as-seq
+         s/first
+         s/rest
+         s/cons
+         s/reduce
+         s/map)
+         
+(struct empty-seq ())
+
+(: null-seq empty-seq)
+(define null-seq (empty-seq))
+
+(: null-seq? (-> Any Boolean))
+(define null-seq? empty-seq?)
+
+(define-type (Seq a) (U empty-seq (Pairof a (-> (Seq a)))))
+
+(define-type (Collection a) (Object (seq (-> (Seq a)))))
+
+(: s/as-seq (All (a) (-> (Collection a) (Seq a))))
+(define (s/as-seq c) (send c seq))
+
+(: s/first (All (a) (-> (Seq a) a)))
+(define (s/first s)
+  (if (empty-seq? s)
+      (error "Sequence is empty")
+      (car s)))
+
+(: s/rest (All (a) (-> (Seq a) (Seq a))))
+(define (s/rest s)
+  (if (empty-seq? s)
+      (error "Sequence is empty")
+      ((cdr s))))
+
+(define-syntax-rule (s/cons x y)
+  (cons x (λ () y)))
+
+(: s/reduce (All (a b) (-> (-> b a b)
+                         (Seq a)
+                         b
+                         b)))
+(define (s/reduce rfn seq start)
+  (if (empty-seq? seq)
+      start
+      (s/reduce rfn (s/rest seq) (rfn start (s/first seq)))))
+
+(: s/map (All (a b) (-> (-> a b)
+                        (Seq a)
+                        (Seq b))))
+(define (s/map mfn seq)
+  (if (null-seq? seq)
+      null-seq
+      (cons (mfn (s/first seq))
+            (λ () : (Seq b)
+              (s/map mfn (s/rest seq))))))
+
+              
+
+
+  
